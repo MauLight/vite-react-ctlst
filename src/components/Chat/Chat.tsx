bd1aa5c76b70db3bb0d8b684b6e1f8b6bbbbe69b
Mau Ulisse Luz <maulisseluz@gmail.com>
2024-11-04 08:33:31 -0300

    feat: added mock conversation
    also fixed issues in chat

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, SetStateAction, useEffect, useState, type ReactElement } from 'react'
+import { Dispatch, RefObject, SetStateAction, useEffect, useRef, useState, type ReactElement } from 'react'
 import user1 from '../../assets/1.webp'
 import user2 from '../../assets/2.webp'
 import user3 from '../../assets/3.webp'
@@ -9,6 +9,7 @@ import user7 from '../../assets/7.webp'
 import user8 from '../../assets/8.webp'
 import user9 from '../../assets/9.webp'
 import user10 from '../../assets/10.webp'
+import chat_bg from '../../assets/chat_bg2.webp'
 
 interface ChatProps {
     chatIsOpen: boolean,
@@ -44,7 +45,9 @@ const upperFirstLetter = (word: string) => {
 export const Chat = ({ chatIsOpen, setChatIsOpen }: ChatProps): ReactElement => {
     const [selectedUser, setSelectedUser] = useState<UserProps | null>(null)
     const [currChatValue, setCurrChatValue] = useState<string>('')
-    const [chatStream, setChatStream] = useState<{ id: number, text: string, date: string, user: string }[]>([])
+    const [chatStream, setChatStream] = useState<{ id: number, text: string, date: string, sender: string, recipient: string }[]>([])
+
+    const scrollRef: RefObject<HTMLDivElement> = useRef(null)
 
     const handleIndividualChat = (user: UserProps) => {
         setSelectedUser(user)
@@ -52,45 +55,96 @@ export const Chat = ({ chatIsOpen, setChatIsOpen }: ChatProps): ReactElement => 
 
     const handleSubmitToChat = (e: { preventDefault: () => void }) => {
         e.preventDefault()
-        setChatStream([...chatStream, { id: chatStream.length, text: currChatValue, date: Date.now().toString(), user: 'me' }])
-        setCurrChatValue('')
+        if (currChatValue !== '') {
+            setChatStream([...chatStream, { id: chatStream.length + 1, text: currChatValue, date: Date.now().toString(), sender: 'me', recipient: selectedUser?.username || '' }])
+            setCurrChatValue('')
+        }
     }
 
     useEffect(() => {
-        console.log(chatStream)
+        if (chatStream.length === 2) {
+            setTimeout(() => {
+                const newMessage = {
+                    id: chatStream.length + 1,
+                    text: 'Hey Mau, what\'s up?',
+                    date: Date.now().toString(),
+                    sender: selectedUser?.username || '',
+                    recipient: 'me'
+                }
+                setChatStream([...chatStream, newMessage])
+            }, 5000)
+            return
+        }
+        if (chatStream.length === 4) {
+            setTimeout(() => {
+                const newMessage = {
+                    id: chatStream.length + 1,
+                    text: 'Sure thing, I wonder what you\'re working on',
+                    date: Date.now().toString(),
+                    sender: selectedUser?.username || '',
+                    recipient: 'me'
+                }
+                setChatStream([...chatStream, newMessage])
+            }, 4000)
+            return
+        }
+        if (chatStream.length === 6) {
+            setTimeout(() => {
+                const newMessage = {
+                    id: chatStream.length + 1,
+                    text: 'Okay 👍',
+                    date: Date.now().toString(),
+                    sender: selectedUser?.username || '',
+                    recipient: 'me'
+                }
+                setChatStream([...chatStream, newMessage])
+            }, 4000)
+            return
+        }
+    }, [chatStream])
+
+    useEffect(() => {
+        const lastDiv = scrollRef.current
+        if (lastDiv !== null) {
+            lastDiv.scrollIntoView({ behavior: 'smooth' })
+        }
     }, [chatStream])
 
     return (
         <div className={`fixed ${chatIsOpen ? 'right-10' : '-right-72'} bottom-10 w-[250px] border rounded-[20px] bg-[#ffffff] shadow-xl z-20 transition-all duration-200`}>
             <i onClick={() => { setChatIsOpen(false) }} className="absolute -top-1 -right-0 fa-solid fa-xmark fa-md hover:rotate-45 hover:text-indigo-500 transition-all duration-200 z-20"></i>
-            <h1 onClick={() => { setSelectedUser(null) }} className='text-center text-gray-800 py-2 hover:text-indigo-500 transition-colors duration-200 cursor-pointer'>Chats</h1>
+            <h1 onClick={() => { setSelectedUser(null); setChatStream([]) }} className='text-center text-gray-800 py-2 hover:text-indigo-500 transition-colors duration-200 cursor-pointer'>Chats</h1>
             <div className='rounded-[20px] h-[350px] p-1 overflow-y-scroll scrollbar-hide'>
 
                 {
                     selectedUser ? (
                         <>
-                            <div className='w-full h-[89%] rounded-b-[15px] border bg-indigo-500 flex flex-col items-start justify-between overflow-hidden'>
-                                <div className="flex items-center gap-x-2 p-2 bg-slate-700 w-full">
+                            <div className='relative w-full h-full rounded-b-[15px] border animated-background bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 flex flex-col items-start justify-between overflow-hidden'>
+                                <img className='absolute h-full w-full z-0 object-cover grayscale opacity-5' src={chat_bg} alt="bg" />
+                                <div className="flex items-center gap-x-2 p-2 z-10 bg-[#40403e] w-full">
                                     <div className='relative flex'>
                                         <div className={`absolute right-0 bottom-0 w-2 h-2 ${selectedUser.active ? 'bg-green-500' : 'bg-transparent'} rounded-full`}></div>
                                         <img className='w-8 h-8 rounded-full object-cover' src={selectedUser.pic} alt="user" />
                                     </div>
                                     <h1 className='text-[0.9rem] text-gray-50'>{upperFirstLetter(selectedUser.username)}</h1>
                                 </div>
-                                <div className='h-full w-full overflow-y-scroll scrollbar-hide py-2'>
+                                <div className='h-full w-full overflow-y-scroll scrollbar-hide py-2 z-10'>
                                     {
                                         chatStream.length > 0 && chatStream.map((text) => (
-                                            <div key={text.id} className={`flex flex-col px-2 py-1 ${text.user === 'me' ? 'items-end' : 'items-start'}`}>
-                                                <div className='px-2 py-1 bg-white rounded-[8px]'>
+                                            <div key={text.id} className={`flex flex-col px-2 py-1 ${text.sender === 'me' ? 'items-end' : 'items-start'}`}>
+                                                <div className={`px-2 py-1 rounded-[8px] shadow-sm ${text.sender === 'me' ? 'bg-indigo-200' : 'bg-gray-50'}`}>
                                                     <p className='text-[0.9rem] text-gray-800'>{text.text}</p>
                                                 </div>
                                             </div>
                                         ))
                                     }
+                                    <div ref={scrollRef}></div>
                                 </div>
-                                <form onSubmit={handleSubmitToChat} className="h-10 w-full flex items-center bg-slate-700 p-2">
+                                <form onSubmit={handleSubmitToChat} className="h-10 w-full flex items-center bg-[#40403e] p-2 z-10">
                                     <input value={currChatValue} onChange={({ target }) => { setCurrChatValue(target.value) }} type="text" className='relative h-6 w-full bg-[#10100e] rounded-full px-2 text-gray-200 text-[0.8rem] outline-none' />
-                                    <i className="absolute right-5 fa-solid fa-paper-plane text-gray-200"></i>
+                                    <button type='submit' className='absolute right-5 '>
+                                        <i className="fa-solid fa-paper-plane text-gray-200"></i>
+                                    </button>
                                 </form>
                             </div>
                         </>
