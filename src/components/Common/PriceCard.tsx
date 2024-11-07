6f5bb6e3a893c31ea11986d49f4938c2a6f7e2a6
Mau Ulisse Luz <maulisseluz@gmail.com>
2024-11-07 04:54:36 -0300

    feat: add responsiveness to pricing screen

--- src/components/Common/PriceCard.tsx
+++ src/components/Common/PriceCard.tsx
@@ -1,31 +1,33 @@
 import { ReactElement } from 'react'
 
 interface PriceCardProps {
-price: number
-saved: number
-percentage: number
-title: string
-description: string
-cta: string
-size?: string
-clicked: boolean
+  price: number
+  saved: number
+  percentage: number
+  title: string
+  description: string
+  cta: string
+  size?: string
+  clicked: boolean
 }
 
-export const PriceCard = ({ price, saved, percentage, title, description, cta, size, clicked } : PriceCardProps): ReactElement => {
+export const PriceCard = ({ price, saved, percentage, title, description, cta, size, clicked }: PriceCardProps): ReactElement => {
   return (
-    <div className={`w-full ${size === 'sm'  ? 'h-full bg-[#40403e]' : size === 'en' ? 'h-full' : 'h-4/5 bg-[#282826] '} flex flex-col justify-between items-start p-5 rounded-[15px]`}>
+    <div className={`w-full ${size === 'sm' ? 'h-[600px] lg:h-full bg-[#40403e]' : size === 'en' ? 'h-[600px] lg:h-full' : 'h-[600px] lg:h-4/5 bg-[#282826] '} flex flex-col justify-between items-start p-5 rounded-[15px]`}>
       <div className={`border border-indigo-500 px-1 rounded-[5px] ${size ? 'opacity-0' : ''}`}>
         <p className='font-body text-[12px] text-indigo-500 uppercase'>Recommended</p>
       </div>
-      <div className="w-full flex flex-col">
-        <div className="w-full flex justify-end items-end gap-x-1">
-          <p className={`font-body text-[32px] ${size === 'en' ? 'text-[#282826]' : 'text-[#ffffff]'} tracking-tight leading-none`}>{`$${price}`}</p>
-          <p className={`font-body text-[16px] ${size === 'en' ? 'text-gray-600' : 'text-gray-300'}`}>{size === 'en' ? 'user/month' : '/ month'}</p>
+      <div className="w-full flex flex-col gap-y-3">
+        <div>
+          <div className="w-full flex justify-end items-end gap-x-1">
+            <p className={`font-body text-[32px] ${size === 'en' ? 'text-[#282826]' : 'text-[#ffffff]'} tracking-tight leading-none`}>{`$${price}`}</p>
+            <p className={`font-body text-[16px] ${size === 'en' ? 'text-gray-600' : 'text-gray-300'}`}>{size === 'en' ? 'user/month' : '/ month'}</p>
+          </div>
+          <div className="w-full flex justify-end items-end gap-x-1">
+            <p className={`font-body text-[12px] ${size === 'en' ? 'text-gray-600' : 'text-gray-300'}`}>{clicked ? '*Billed anually' : '*Billed monthly'}</p>
+          </div>
         </div>
-        <div className="w-full flex justify-end items-end gap-x-1">
-          <p className={`font-body text-[12px] ${size === 'en' ? 'text-gray-600' : 'text-gray-300'}`}>{clicked ? '*Billed anually' : '*Billed monthly'}</p>
-        </div>
-        <div className="w-full flex justify-between items-end gap-x-1 pt-1">
+        <div className="w-full flex max-[1370px]:flex-col justify-between items-end max-[1370px]:gap-y-2 gap-x-1 pt-1">
           <div className="flex items-end gap-x-2">
             <p className={`uppercase font-body text-[20px] ${size === 'en' ? 'text-[#282826]' : 'text-[#ffffff]'} leading-none`}>{title.split(' ')[0]}</p>
             <p className={`font-body text-[18px] ${size === 'en' ? 'text-[#282826]' : 'text-[#ffffff]'} leading-none`}>{title.split(' ')[1]}</p>
@@ -36,7 +38,7 @@ export const PriceCard = ({ price, saved, percentage, title, description, cta, s
           </div>
         </div>
         <div className="w-full flex justify-between items-end gap-x-1 pt-5">
-          <p className={`text-[14px] text-balance ${size === 'en' ? 'text-gray-600 font-body' : 'text-gray-300 font-light'} leading-none w-2/3`}>
+          <p className={`text-[1.1rem]  lg:text-[14px] text-balance ${size === 'en' ? 'text-gray-600 font-body' : 'text-gray-300 font-light'} leading-none lg:w-2/3`}>
             {description}
           </p>
         </div>
