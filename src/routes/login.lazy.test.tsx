8ac5ea2e86437bbdb6dcca71ec9edddb49ac2457
Mau Ulisse Luz <maulisseluz@gmail.com>
2024-11-18 23:54:01 -0300

    feat: prepare migration to react-router

---
+++ src/routes/login.lazy.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen, fireEvent } from '@testing-library/react'
+import { describe, it, expect, vi } from 'vitest'
+//import { Login } from './login.lazy'
+
+// describe('Login component', () => {
+//     it('renders the login form', () => {
+//         render(<Login />)
+//         expect(screen.getByText(''))
+//     })
+// })
